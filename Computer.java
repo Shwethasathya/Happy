d32291755bccdb2ef5feb564bfe7398d4fa13f66
unknown <sshwetha.xworkz@gmail.com>
2022-09-14 10:50:26 +0530

    memory

---
+++ Computer.java
@@ -0,0 +1,17 @@
+class Computer{
+	
+	public static void main(String[] args)
+	{
+		char a1, a2, a3, a4;
+		
+		
+		System.out.println("Ascii characters");
+		
+		a1=83;
+		a2=72;
+		a3=87;
+		a4=69;
+		
+		System.out.println( "Ascii characters are "  + a1 + a2 + a3 + a4);
+	}
+}
