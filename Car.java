a66a6bc1e695ecacb5162ce7f1320cfd1a51d3f7
unknown <sshwetha.xworkz@gmail.com>
2022-09-14 10:38:03 +0530

    hi

---
+++ Car.java
@@ -0,0 +1,36 @@
+class Car{
+	
+	public static void main(String[] interesting)
+	
+	{
+		boolean sedanBrand = true;
+		
+		long price = 4260000;
+		
+		byte noofdoors = 4;
+		
+		char model = 'Y';
+		
+		System.out.println("is the car is sedan " + true);
+		
+		System.out.println("price of the car is " + 4260000);
+		
+		System.out.println("number of doors in the car is " + noofdoors);
+		
+		System.out.println("model of the car is " + 'Y');
+		
+		
+		char dmodel = 'N';
+		
+		byte seatcapacity = 4;
+		
+		boolean audiBrand = false;
+		
+		System.out.println("model " + 'N');
+		
+		System.out.println("seat capacity is " + 4);
+		
+		System.out.println("is the car is sedan " + false);
+		
+	}
+}
