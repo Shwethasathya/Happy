6b523eebce763e0e15513f9f33e6fc6097a0df8a
Shwethasathya <113284867+Shwethasathya@users.noreply.github.com>
2022-09-20 07:37:57 +0530

    Add files via upload

---
+++ SportsRunner.java
@@ -0,0 +1,27 @@
+class SportsRunner{
+
+public static void main(String[] args)
+{
+String[] cricket={"Rohit sharma","Rishabh pant","lokesh rahul","bhuvneshwar kumar","yuzvendra chahal","deepak hooda","virat kohli","umesh yadav","jasprit bumrah",};
+Sports.cricketTeamMembers(cricket);
+
+int[] kabbadi={10, 1, 7, 4, 18, 2};
+Sports.kabbadiTeamMembersJerseyNos(kabbadi);
+
+double[] football={80000d, 100000d,210000d};
+Sports.FootBallTeamMembersSalary(football);
+
+long[] ludo={9765210230L,7411944092L,7411965003L,9741638606L,6820120340L};
+Sports.ludoTeamMobileNumber(ludo);
+
+int[] hockey={8};
+Sports.hockeyTeamMembersNoOfMatches(hockey);
+
+long[] koko={120, 125};
+Sports.kokoTeamMembersCountryCode(koko);
+
+boolean[] lagori={true,false};
+Sports.lagoriTeamMembersAlive(lagori);
+}
+
+}
