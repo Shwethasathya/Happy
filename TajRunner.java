bd2bb6626e50f052f091fd98a0736d9262b9691e
Shwethasathya <113284867+Shwethasathya@users.noreply.github.com>
2022-09-23 06:40:56 +0530

    Add files via upload

---
+++ TajRunner.java
@@ -0,0 +1,39 @@
+class TajRunner
+{
+public static void main(String[] information)
+	{
+	
+		System.out.println("main: entering agara fort information");
+	
+		String place=information[0];
+        System.out.println(" place in index 0 : "+place);
+		
+		String built=information[1];
+        System.out.println(" place in index 1 : "+built);
+		
+		String address=information[2];
+        System.out.println("address in index 2 :"+address);
+		
+		String style=information[3];
+        System.out.println("style in index 3 :"+style);
+		
+		String phone=information[4];
+        System.out.println("phone number in index 4 :"+phone);
+		
+		String height=information[5];
+        System.out.println("height in index 5 :"+height);
+		
+		String area=information[6];
+        System.out.println("area in index 6 :"+area);
+		
+		String builtfor=information[7];
+        System.out.println("built for in index 7 :"+builtfor);
+		
+		String material=information[8];
+        System.out.println("buit material in index 8 :"+material);
+        
+        System.out.println("information :"+information.length);
+
+}
+
+}
