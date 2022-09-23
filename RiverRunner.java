b9f07cfe9bd7485d6252eaa2a7daedbd90072735
Shwethasathya <113284867+Shwethasathya@users.noreply.github.com>
2022-09-23 07:51:09 +0530

    method

---
+++ RiverRunner.java
@@ -0,0 +1,47 @@
+class RiverRunner{
+
+public static void main(String[] ganga)
+{
+	
+	System.out.println("main : entering River");
+	
+		String ref=ganga[0];
+        System.out.println("index 0 :"+ref);
+        
+        String ref1=ganga[1];
+        System.out.println("index 1 :"+ref1);
+		
+		String ref2=ganga[2];
+        System.out.println("index 2 :"+ref2);
+		
+		String ref3=ganga[3];
+        System.out.println("index 3 :"+ref3);
+		
+		String ref4=ganga[4];
+        System.out.println("index 4 :"+ref4);
+        
+		String ref5=ganga[5];
+        System.out.println("index 5 :"+ref5);
+		
+		String ref6=ganga[6];
+        System.out.println("index 6 :"+ref6);
+		
+		String ref7=ganga[7];
+        System.out.println("index 7 :"+ref7);
+		
+		String ref8=ganga[8];
+        System.out.println("index 8 :"+ref8);
+		
+		String ref9=ganga[9];
+        System.out.println("index 9 :"+ref9);
+		
+		String ref10=ganga[10];
+        System.out.println("index 10 :"+ref10);
+		
+        System.out.println("River :"+ ganga.length);
+	
+	System.out.println("Exit of main method");
+	
+}
+
+}
